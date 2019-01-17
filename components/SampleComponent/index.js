d851dc6eb3879fec30ecfb36c26e1a40f2291e77
revh <lorenzo.rev@gmail.com>
2019-01-17 11:36:21 +0100

    Initial commit

---
+++ components/SampleComponent/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const SampleComponent = styled.button`
+  color: yellow;
+  border: none;
+  background-color: pink;
+  padding: 10px;
+`
+
+export default SampleComponent
