296e48a1498233ecb8eb6098c1c4836a79ad2789
eatsjobs <pasquale.mangialavori@gmail.com>
2019-01-18 10:17:38 +0100

    add rollup for bundling

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # lab-chat
+
+# Peer dependencies (to be installed in the consumer package)
+    "@fortawesome/fontawesome-svg-core": "^1.2.12",
+    "@fortawesome/free-solid-svg-icons": "^5.6.3",
+    "@fortawesome/react-fontawesome": "^0.1.4",
+    "moment": "^2.23.0",
+    "styled-components": "^4.1.3",
+    "react": "^16.4.1",
+    "react-dom": "^16.4.1"
---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import pkg from './package.json';
+
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import image from 'rollup-plugin-img';
+// babel
+import babel from 'rollup-plugin-babel';
+
+// peer deps
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import fs from 'fs';
+import path from 'path';
+
+export default [
+
+	// CommonJS (for Node) and ES module (for bundlers) build.
+	// (We could have three entries in the configuration array
+	// instead of two, but it's quicker to generate multiple
+	// builds from a single configuration where possible, using
+	// an array for the `output` option, where we can specify 
+	// `file` and `format` for each target)
+	{
+		input: 'components/index.js',
+		
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' },
+            {
+			    name: 'chat-ui',
+			    file: pkg.browser,
+			    format: 'umd'
+            }
+		],
+        plugins: [
+            peerDepsExternal(),
+            image({
+                output: `lib/`, // default the root
+                extensions: /\.(png|jpg|jpeg|gif|svg)$/, // support png|jpg|jpeg|gif|svg, and it's alse the default value
+                limit: 8192,  // default 8192(8k)
+                exclude: 'node_modules/**'
+            }),
+            resolve(), // so Rollup can find `ms`
+            babel({ exclude: 'node_modules/**' }),
+            commonjs()
+		]
+	}
+];
