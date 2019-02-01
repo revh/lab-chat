b6806c30c1c0a55bb75a76086f6b51405ccec944
eatsjobs <pasquale.mangialavori@gmail.com>
2019-02-01 13:55:51 +0100

    fixed tests

--- components/SearchBox/index.spec.js
+++ components/SearchBox/index.spec.js
@@ -5,20 +5,24 @@ describe('<SeachBox />', () => {
     it('should render', () => {
         const onChangeMock = jest.fn();
         const onSubmitMock = jest.fn();
-        const wrapper = shallow(
+        const wrapper = mount(
             <SearchBox
                 value=''
                 onChange={onChangeMock}
                 onSubmit={onSubmitMock}
                 placeholder='search'
+                autoComplete='off'
             />);
         const form = wrapper.find(SearchContainer);
         const input = wrapper.find(SearchInput);
-        wrapper.setProps({ value: 'pippo' });
-        //console.log(wrapper.debug());
         expect(form.length).toBe(1);
-        form.simulate('submit', { key: 'Enter', keyCode: 13 })
+        /** 
+         * event does not propagate if you use shallow
+         */
+        // input.simulate('keyup', { keyCode: 13, key: 'Enter' });
+        input.simulate('submit', { key: 'Enter', keyCode: 13 });
         expect(onSubmitMock).toHaveBeenCalled();
+        // expect(onChangeMock).toHaveBeenCalled();
     });
 
    it('should meet accessibility guidelines', async () => {
